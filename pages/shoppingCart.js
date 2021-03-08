f4fa9dc2c15c605ab727283d7fe8e278d6991ac3
Emilia Tulan <emilia.tulan@gmail.com>
2021-03-08 16:40:23 +0100

    Fix typo layout

--- pages/shoppingCart.js
+++ pages/shoppingCart.js
@@ -3,18 +3,13 @@ import Head from 'next/head';
 import Layout from '../components/Layout';
 
 export default function ShoppingCart(props) {
-  const cookieMonster = props.cookie.map((element) => {
-    return element.id;
-  });
-  const idFromDataBase = props.singleWatch.map((watch) => {
-    return watch.id;
-  });
-
-  // function cookieIdVsDataBaseId() { let cookieAndDataBase;
-  //   if (cookieMonster === idFromDataBase) return watch.productName};
-
-  // console.log(idFromDataBase);
-
+  function finalPrice() {
+    let totalPrice = 0;
+    props.productFinalToBeOrdered.forEach((element) => {
+      totalPrice = element.price * element.quantity;
+    });
+    return totalPrice;
+  }
   return (
     <>
       <Layout>
@@ -25,15 +20,20 @@ export default function ShoppingCart(props) {
       </Layout>
       <div>Shopping Cart</div>
 
-      {/* {props.cookie.map((element) => {
+      {props.productFinalToBeOrdered.map((element) => {
         return (
-          <>
+          <div key={element.id}>
             {' '}
-            <p>{element.id} </p>
-            <p>{element.quantity}</p>{' '}
-          </>
+            <p>Product: {element.productName} </p>
+            <p>Price: {element.price} € </p>
+            <p>Quantity: {element.quantity} </p>
+            <p>
+              {' '}
+              <strong>Total: {finalPrice()} € </strong>{' '}
+            </p>
+          </div>
         );
-      })} */}
+      })}
 
       <p>Your products are ready for checkout:</p>
     </>
@@ -41,36 +41,34 @@ export default function ShoppingCart(props) {
 }
 
 export async function getServerSideProps(context) {
-  const cookie = nextCookies(context).watchesCookie;
   const { getWatchesDataBase } = await import('../util/database.js');
+  const allWatches = await getWatchesDataBase();
 
-  const singleWatch = await getWatchesDataBase();
+  const cookie = nextCookies(context);
 
-  const cookieMonster = cookie.map((element) => {
-    return element.id;
+  const productFinalToBeOrdered = [];
+
+  cookie.watchesCookie.forEach((watchToBeOrdered) => {
+    const singleProduct = allWatches.find(
+      (watch) => watch.id === watchToBeOrdered.id,
+    );
+
+    if (singleProduct !== undefined) {
+      const productToBeOrdered = {
+        watchesId: watchToBeOrdered.id,
+        productName: singleProduct.productName,
+        price: singleProduct.price,
+        quantity: watchToBeOrdered.quantity,
+      };
+      if (watchToBeOrdered.quantity !== 0) {
+        productFinalToBeOrdered.push(productToBeOrdered);
+      }
+    }
   });
 
-  console.log('monster cookie', cookieMonster);
-  const idFromDataBase = singleWatch.map((watch) => {
-    return watch.id;
-  });
-  console.log('id from database', idFromDataBase);
-
-  //const productToBeOrder = ;
-
-  //console.log ("selected products", productToBeOrder);
-
-  // 1. loop over the cookie and get the id of the product inside of the cookie
-  //2. get from singleWatch just the ID
-  // add to objects that I have in 2 the quantity that i get from number 1
-  // result: array with only the objects the watches I need, and this objects to have the quantity
   return {
     props: {
-      cookie: cookie || null,
-      singleWatch: singleWatch || null,
-      // new props and the ones above i wont need them
+      productFinalToBeOrdered: productFinalToBeOrdered,
     },
   };
 }
-
-// { watchesCookie: [ { id: 2, quantity: 3 }, { id: 4, quantity: 1 } ] }
--- pages/watches/[id].js
+++ pages/watches/[id].js
@@ -1,90 +1,89 @@
 import Cookies from 'js-cookie';
 import Head from 'next/head';
 import { useState } from 'react';
-import Layout from '../../components/layout';
+import Layout from '../../components/Layout';
 
-export default function getSingleProduct(props) {
-  const [watch, setWatch] = useState(props.singleWatch);
+export default function GetSingleProduct(props) {
+  const watch = props.singleWatch;
   const [addWatchInCart, setAddWatchInCart] = useState(1);
 
   return (
-    <>
-      <Layout>
-        <Head>
-          <link rel="logo" href="/logo.svg" />
-          <title>Single watch</title>
-        </Head>
-        <section>
-          <h1>
-            Name:
-            {watch.productName}
-          </h1>
-          <div>About collection: {watch.aboutCollection}</div>
-          <div> Description: {watch.description}</div>
-          <div> Price: {watch.price} €</div>
-        </section>
+    <Layout>
+      <Head>
+        <link rel="logo" href="/logo.svg" />
+        <title>Single watch</title>
+      </Head>
+      <section>
+        <h1>
+          Name:
+          {watch.productName}
+        </h1>
+        <div>About collection: {watch.aboutCollection}</div>
+        <div> Description: {watch.description}</div>
+        <div> Price: {watch.price} €</div>
+      </section>
 
-        <button
-          onClick={() => {
-            let cookieValueClientSide = Cookies.get('watchesCookie');
-            if (cookieValueClientSide) {
-              cookieValueClientSide = JSON.parse(cookieValueClientSide);
-            }
+      <button
+        onClick={() => {
+          let cookieValueClientSide = Cookies.get('watchesCookie');
+          if (cookieValueClientSide) {
+            cookieValueClientSide = JSON.parse(cookieValueClientSide);
+          }
 
-            if (cookieValueClientSide && cookieValueClientSide.length > 0) {
-              cookieValueClientSide.map((singleWatchInCart) => {
-                if (watch.id === singleWatchInCart.id) {
-                  singleWatchInCart.quantity += addWatchInCart;
-                  return singleWatchInCart;
-                }
-              });
-
-              if (
-                cookieValueClientSide.find((element) => {
-                  return element.id === watch.id;
-                }) === undefined
-              ) {
-                cookieValueClientSide.push({
-                  id: watch.id,
-                  quantity: addWatchInCart,
-                });
+          if (cookieValueClientSide && cookieValueClientSide.length > 0) {
+            cookieValueClientSide.map((singleWatchInCart) => {
+              if (watch.id === singleWatchInCart.id) {
+                singleWatchInCart.quantity += addWatchInCart;
               }
-              Cookies.set('watchesCookie', cookieValueClientSide);
-            } else {
-              Cookies.set(
-                'watchesCookie',
-                JSON.stringify([{ id: watch.id, quantity: addWatchInCart }]),
-              );
-            }
-          }}
-        >
-          Add to Cart{' '}
-        </button>
-        <p>Number cookies: {addWatchInCart}</p>
-        <button
-          onClick={() => {
-            addWatchInCart > 0 ? setAddWatchInCart(addWatchInCart - 1) : '';
-          }}
-        >
-          -
-        </button>
+              return singleWatchInCart;
+            });
 
-        <button
-          onClick={() => {
-            setAddWatchInCart(addWatchInCart + 1);
-          }}
-        >
-          +
-        </button>
-      </Layout>
-    </>
+            if (
+              cookieValueClientSide.find((element) => {
+                return element.id === watch.id;
+              }) === undefined
+            ) {
+              cookieValueClientSide.push({
+                id: watch.id,
+                quantity: addWatchInCart,
+              });
+            }
+            Cookies.set('watchesCookie', cookieValueClientSide);
+          } else {
+            Cookies.set(
+              'watchesCookie',
+              JSON.stringify([{ id: watch.id, quantity: addWatchInCart }]),
+            );
+          }
+        }}
+      >
+        Add to Cart{' '}
+      </button>
+      <p>Number cookies: {addWatchInCart}</p>
+      <button
+        onClick={() => {
+          return addWatchInCart > 0
+            ? setAddWatchInCart(addWatchInCart - 1)
+            : '';
+        }}
+      >
+        -
+      </button>
+
+      <button
+        onClick={() => {
+          setAddWatchInCart(addWatchInCart + 1);
+        }}
+      >
+        +
+      </button>
+    </Layout>
   );
 }
 
 export async function getServerSideProps(context) {
   const { getWatchesDataBaseId } = await import('../../util/database.js');
   const idUrl = context.query.id;
-  console.log('whatis this', idUrl);
   const singleWatch = await getWatchesDataBaseId(idUrl);
 
   return {
